ab85d5c090286fe1f6e7ab865590fcaa92bc7f2d
rav3434 <ravish_cg@yahoo.com>
2022-11-04 15:32:18 +0530

    new

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ const conn = mongoose.createConnection(process.env.MONGO_KEY,{ useNewUrlParser: 
 
 
 app.use(cors(), function(req, res, next) {
-    res.header("Access-Control-Allow-Origin","https://employeemangement.netlify.app/"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Origin","https://employeemangement.netlify.app"); // update to match the domain you will make the request from
     res.header(
       "Access-Control-Allow-Headers",
       "Origin, X-Requested-With, Content-Type, Accept"
